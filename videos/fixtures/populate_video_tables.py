b8bff19dd8aee0fd75b0d0a7037cd5f8b4653ff6
cameron <cameroncairns@gmail.com>
2016-04-18 17:32:38 -0700

    Changes script to work with new database schema. Adds relationship creation for all possible user relationships

--- videos/fixtures/populate_video_tables.py
+++ videos/fixtures/populate_video_tables.py
@@ -1,12 +1,17 @@
 import itertools
-import os
 import json
+import os
+import random
 
 import requests
 
-from profiles.models import User
-from videos.models import Category, Video
+from profiles.models import User, Profile
+from videos.models import Category, Video, Tag
 from videos.mixins import VideoAPIMixin
+
+def exclude_user(user_ids, id_to_exclude):
+    id_index = user_ids.index(id_to_exclude)
+    return user_ids[:id_index] + user_ids[id_index+1:]
 
 def populate_category_table():
     # By inspection I found that categories are the same across supported
@@ -35,6 +40,14 @@ def populate_user_table():
                   for index 
                   in range(100)]
     User.objects.bulk_create(fake_users)
+    user_ids = list(User.objects.values_list('id', flat=True))
+    # Exclude first_user id as a profile has already been created for it
+    user_ids.remove(first_user.id)
+    # Create profiles for all bulk created users
+    profiles = [Profile(user_id=id)
+                for id
+                in user_ids]
+    Profile.objects.bulk_create(profiles)
 
 def populate_video_table():
     user_ids = User.objects.values_list('id', flat=True)
@@ -58,3 +71,26 @@ def populate_video_table():
     for index in range(0, len(video_ids), 50):
         Video.objects.create_videos(user_ids[index%50],
                                     *video_ids[index:index+50])
+
+def populate_user_relationships():
+    """
+    Creates random relationships for users with other users, categories, tags
+    and videos
+    """
+    id_objects = [Category, User, Tag, Video]
+    id_map = map(lambda x:x.objects.values_list('id', flat=True), id_objects)
+    category_ids, user_ids, tag_ids, video_ids = [list(query)
+                                                  for query
+                                                  in list(id_map)]
+    for user in User.objects.iterator():
+        user.favorite_videos.add(*random.sample(video_ids, 10))
+        user.followed_categories.add(*random.sample(category_ids, 10))
+        user.followed_tags.add(*random.sample(tag_ids, 50))
+        user.following.add(*random.sample(exclude_user(user_ids, user.id), 5))
+
+
+def populate_tables():
+    populate_category_table()
+    populate_user_table()
+    populate_video_table()
+    populate_user_relationships()
