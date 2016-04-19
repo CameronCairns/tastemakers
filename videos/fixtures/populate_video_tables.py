4abf43d64503d0c20127625f49cc042b649f8e4b
cameron <cameroncairns@gmail.com>
2016-04-18 19:39:24 -0700

    Adds fake-factory plugin to help generate fixture data. Modified populate_video_tables script to generate user relations and comments

--- videos/fixtures/populate_video_tables.py
+++ videos/fixtures/populate_video_tables.py
@@ -1,13 +1,17 @@
+import pdb
 import itertools
 import json
 import os
 import random
 
 import requests
+import faker
 
 from profiles.models import User, Profile
-from videos.models import Category, Video, Tag
+from videos.models import Category, Comment, Tag, Video
 from videos.mixins import VideoAPIMixin
+
+fake = faker.Factory.create()
 
 def exclude_user(user_ids, id_to_exclude):
     id_index = user_ids.index(id_to_exclude)
@@ -27,15 +31,15 @@ def populate_category_table():
 def populate_user_table():
     # Create the first fake user from which a hashed password will be gathered
     # for future entries
-    first_user = User.objects.create_user('first user',
-                                          'firstuser@tastemakers.com',
-                                          'fake_password')
+    first_user = User.objects.create_user(fake.username(),
+                                          fake.email(),
+                                          fake.password())
     first_user.save()
     hashed_password = first_user.password
-    fake_users = [User(username='user{}'.format(index),
-                       first_name='user{}first'.format(index),
-                       last_name='user{}last'.format(index),
-                       email='user{}@tastemakers.com'.format(index),
+    fake_users = [User(username=fake.username(),
+                       first_name=fake.first_name(),
+                       last_name=fake.last_name(),
+                       email=fake.email(),
                        password=hashed_password)
                   for index 
                   in range(100)]
@@ -88,9 +92,29 @@ def populate_user_relationships():
         user.followed_tags.add(*random.sample(tag_ids, 50))
         user.following.add(*random.sample(exclude_user(user_ids, user.id), 5))
 
+def populate_comment_table():
+    user_ids = list(User.objects.values_list('id', flat=True))
+    video_ids = list(Video.objects.values_list('id', flat=True))
+    # Generate comments from all users for all videos
+    video_comments = [Comment(text=fake.text(),
+                              commenter_id=user_id,
+                              video_id=video_id)
+                      for user_id in user_ids
+                      for video_id in video_ids]
+    Comment.objects.bulk_create(video_comments)
+    # Generate random comments to comments from all users
+    for number_of_generations in range(1000):
+        comments = [Comment(text=fake.text(),
+                            commenter_id=random.choice(user_ids),
+                            parent_id=comment.id,
+                            video_id=comment.video_id)
+                    for comment
+                    in Comment.objects.all().order_by('?')[:100]]
+        Comment.objects.bulk_create(comments)
 
 def populate_tables():
     populate_category_table()
     populate_user_table()
     populate_video_table()
     populate_user_relationships()
+    populate_comment_table()
