a52307a559ca550136e93d925caea472a336c2a0
cameron <cameroncairns@gmail.com>
2016-04-18 19:36:25 -0700

    Adds new models to profiles and videos app admins

--- videos/admin.py
+++ videos/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from videos.models import Category, Video, ViewCount, Tag
+from videos.models import Category, Comment, Video, ViewCount, Vote, Tag
 
 # Register your models here.
 admin.site.register(Category)
+admin.site.register(Comment)
 admin.site.register(Video)
 admin.site.register(ViewCount)
+admin.site.register(Vote)
 admin.site.register(Tag)
