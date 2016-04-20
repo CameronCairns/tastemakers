9b31c9dfb737ab36ebf3a61f29713c5e0efa1414
cameron <cameroncairns@gmail.com>
2016-04-19 21:45:58 -0700

    Changes name of populate_video_tables to reflect the broader scope of the function. Changes populate script to generate video view counts for the past, generate video counts, and populate the data for user profiles

--- scripts/populate_table_data.py
+++ scripts/populate_table_data.py
@@ -1,23 +1,29 @@
-import pdb
+from datetime import timedelta
 import itertools
-import json
-import os
 import random
 
-import requests
 import faker
 
 from profiles.models import User, Profile
-from videos.models import Category, Comment, Tag, Video, Vote
+from videos.models import (Category, Comment, CommentVote, Tag, Video,
+                           VideoVote, ViewCount)
 from videos.mixins import VideoAPIMixin
 
 fake = faker.Factory.create()
 
+
 def exclude_user(user_ids, id_to_exclude):
+    """
+    Helper function to simplify excluding a user_id from a list
+    """
     id_index = user_ids.index(id_to_exclude)
     return user_ids[:id_index] + user_ids[id_index+1:]
 
+
 def populate_category_table():
+    """
+    Populate youtube categories from the category list api call for the US
+    """
     # By inspection I found that categories are the same across supported
     # youtube regions
     parameters = dict(part='snippet', regionCode='US')
@@ -28,38 +34,52 @@ def populate_category_table():
                   in JSON['items']]
     Category.objects.bulk_create(categories)
 
+
 def populate_user_table():
+    """
+    Create 100 random users to associate with video submissions, comments, etc
+    done using bulk create with the same password hash as the first user
+    created normally
+    """
     # Create the first fake user from which a hashed password will be gathered
     # for future entries
-    first_user = User.objects.create_user(fake.username(),
+    first_user = User.objects.create_user(fake.user_name(),
                                           fake.email(),
                                           fake.password())
     first_user.save()
+    first_user.profile.blurb = fake.text
+    first_user.profile.website = fake.url
+    first_user.profile.save()
     hashed_password = first_user.password
-    fake_users = [User(username=fake.username(),
+    fake_users = [User(username=fake.user_name(),
                        first_name=fake.first_name(),
                        last_name=fake.last_name(),
                        email=fake.email(),
                        password=hashed_password)
-                  for index 
-                  in range(100)]
+                  for index
+                  in range(99)]
     User.objects.bulk_create(fake_users)
     user_ids = list(User.objects.values_list('id', flat=True))
     # Exclude first_user id as a profile has already been created for it
     user_ids.remove(first_user.id)
     # Create profiles for all bulk created users
-    profiles = [Profile(user_id=id)
+    profiles = [Profile(user_id=id, blurb=fake.text, website=fake.url)
                 for id
                 in user_ids]
     Profile.objects.bulk_create(profiles)
 
+
 def populate_video_table():
+    """
+    Populate video table by querying 50 videos from each category from
+    youtube
+    """
     user_ids = User.objects.values_list('id', flat=True)
     parameters = dict(part='id',
                       fields='items/id/videoId',
-                      maxResults=50, 
+                      maxResults=50,
                       # Don't need to be embarassed while testing
-                      safeSearch='strict', 
+                      safeSearch='strict',
                       type='video',
                       # Only want to allow embeddable videos for dev database
                       videoEmbeddable='true')
@@ -68,13 +88,14 @@ def populate_video_table():
                  for category_id
                  in Category.objects.values_list('id', flat=True)]
     video_ids = list(itertools.chain(*[[data['id']['videoId']
-                                      for data
-                                      in JSON['items']]
-                                     for JSON
-                                     in responses]))
+                                        for data
+                                        in JSON['items']]
+                                       for JSON
+                                       in responses]))
     for index in range(0, len(video_ids), 50):
-        Video.objects.create_videos(user_ids[index%50],
+        Video.objects.create_videos(user_ids[index % 50],
                                     *video_ids[index:index+50])
+
 
 def populate_user_relationships():
     """
@@ -82,7 +103,7 @@ def populate_user_relationships():
     and videos
     """
     id_objects = [Category, User, Tag, Video]
-    id_map = map(lambda x:x.objects.values_list('id', flat=True), id_objects)
+    id_map = map(lambda x: x.objects.values_list('id', flat=True), id_objects)
     category_ids, user_ids, tag_ids, video_ids = [list(query)
                                                   for query
                                                   in list(id_map)]
@@ -92,7 +113,13 @@ def populate_user_relationships():
         user.followed_tags.add(*random.sample(tag_ids, 50))
         user.following.add(*random.sample(exclude_user(user_ids, user.id), 5))
 
+
 def populate_comment_table():
+    """
+    Generate comment data by having each user generate comments for each
+    video and then randomly generate 1000 comments on randomly selected
+    existing comments
+    """
     user_ids = list(User.objects.values_list('id', flat=True))
     video_ids = list(Video.objects.values_list('id', flat=True))
     # Generate comments from all users for all videos
@@ -112,24 +139,76 @@ def populate_comment_table():
                     in Comment.objects.all().order_by('?')[:100]]
         Comment.objects.bulk_create(comments)
 
+
 def populate_user_votes_comments():
-    video_ids = list(Video.objects.values_list('id', flat=True))
+    """
+    Populate user voting data by having each user randomly vote for
+    10,000 comments.
+    """
     comment_ids = list(Comment.objects.values_list('id', flat=True))
     user_ids = list(User.objects.values_list('id', flat=True))
     for user_id in user_ids:
-        comments = random.sample(comment_ids, 10000) # 10,000 is 5% of 200,000
-        positive_votes = [Vote(value=1,
-                               comment_id=comment_id,
-                               voter_id=user_id)
+        comments = random.sample(comment_ids, 10000)  # 10,000 is 5% of 200,000
+        positive_votes = [CommentVote(value=1,
+                                      comment_id=comment_id,
+                                      voter_id=user_id)
                           for comment_id
                           in comments[:5000]]
-        negative_votes = [Vote(value=-1,
-                               comment_id=comment_id,
-                               voter_id=user_id)
+        negative_votes = [CommentVote(value=-1,
+                                      comment_id=comment_id,
+                                      voter_id=user_id)
                           for comment_id
                           in comments[5000:]]
         votes = itertools.chain(positive_votes, negative_votes)
-        Vote.objects.bulk_create(votes)
+        CommentVote.objects.bulk_create(votes)
+
+
+def populate_user_votes_videos():
+    """
+    Populate user voting data by having each user randomly vote for
+    50 videos
+    """
+    video_ids = list(Video.objects.values_list('id', flat=True))
+    user_ids = list(User.objects.values_list('id', flat=True))
+    for user_id in user_ids:
+        videos = random.sample(video_ids, 50)  # 50 is 5% of 1,000
+        positive_votes = [VideoVote(value=1,
+                                    video_id=video_id,
+                                    voter_id=user_id)
+                          for video_id
+                          in videos[:5000]]
+        negative_votes = [VideoVote(value=-1,
+                                    video_id=video_id,
+                                    voter_id=user_id)
+                          for video_id
+                          in videos[5000:]]
+        votes = itertools.chain(positive_votes, negative_votes)
+        VideoVote.objects.bulk_create(votes)
+
+
+def populate_video_viewcounts():
+    """
+    Simulate video comment viewcounts by subtracting a random amount of views
+    from a videos intial viewcount and associating the difference with a day
+    before the day the viewcount was gathered from. Continuing until the count
+    goes to zero
+    """
+    past_views = []
+    for viewcount in ViewCount.objects.iterator():
+        end_time = viewcount.count_datetime
+        video_id = viewcount.video_id
+        count = viewcount.views
+        viewcounts = []
+        while count > 0:
+            count -= random.randint(0, count)
+            viewcounts.append(count)
+        past_views.append([ViewCount(video_id=video_id,
+                                     count_datetime=(
+                                         end_time - timedelta(days=day+1)),
+                                     views=count)
+                           for day, count
+                           in enumerate(viewcounts)])
+    ViewCount.objects.bulk_create(itertools.chain(*past_views))
 
 
 def populate_tables():
@@ -138,3 +217,6 @@ def populate_tables():
     populate_video_table()
     populate_user_relationships()
     populate_comment_table()
+    populate_user_votes_comments()
+    populate_user_votes_videos()
+    populate_video_viewcounts()
