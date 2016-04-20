32e8e7622575f62041e633f8ebf831f5cf72c3fd
cameron <cameroncairns@gmail.com>
2016-04-20 13:46:27 -0700

    Changes serializers to have videos tags represented by their respective title

--- videos/serializers.py
+++ videos/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
-from .models import Video
+from .models import Tag, Video
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('title',)
 
 
 class VideoSerializer(serializers.ModelSerializer):
+    # tags = TagSerializer(read_only=True, many=True)
+    tags = serializers.SlugRelatedField(many=True,
+                                        read_only=True,
+                                        slug_field='title')
+
     class Meta:
         model = Video
         fields = ('title', 'uploader', 'description', 'video_id', 'category',
