9752b5e741abba72771add6ee81d8a71fa54c183
cameron <cameroncairns@gmail.com>
2016-04-22 00:03:02 -0700

    Adds comment view for videos as well as its template. Modified the url file to include the video comment field

--- videos/urls.py
+++ videos/urls.py
@@ -5,5 +5,8 @@ from videos import views
 urlpatterns = [
         url(r'^videos/(?P<list_by>[-\w]+)/$',
             views.VideoListView.as_view(),
-            name='videos_video_list')
+            name='videos_video_list'),
+        url(r'^videos/(?P<video_id>\d+)/comments$',
+            views.CommentListView.as_view(),
+            name='videos_video_comments')
         ]
--- videos/views.py
+++ videos/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView
 
-from .models import Video
+from .models import Comment, Video
 
 
 class VideoListView(ListView):
@@ -13,6 +13,11 @@ class VideoListView(ListView):
                       submission='created',
                       publication='published')
 
+    TITLE_DICT = dict(likes='Most Liked Videos',
+                      views='Most Viewed Vidoes',
+                      submission='Most Recent User Submissions',
+                      publication='Most Recently Published Videos')
+
     def get_queryset(self):
         """
         Override queryset to allow querying and filtering by custom manager
@@ -24,3 +29,35 @@ class VideoListView(ListView):
             return query()
         else:
             return Video.objects.order_by(query)
+
+    def get_context_data(self, **kwargs):
+        context = super(VideoListView, self).get_context_data(**kwargs)
+        first_video_index = context['page_obj'].start_index()
+        video_indices = [first_video_index + current_index
+                         for current_index
+                         in range(0, len(context['object_list']))]
+        context['object_list'] = list(zip(video_indices,
+                                          context['object_list']))
+        context['title'] = self.TITLE_DICT.get(self.kwargs['list_by'],
+                                               self.TITLE_DICT['likes'])
+        return context
+
+
+class CommentListView(ListView):
+    model = Video
+    template_name = 'videos/video-comments.html'
+
+    def get_queryset(self):
+        """
+        Override base queryset method to allow for querying by the video id
+        the comments are related to and structuring the data to allow for
+        child parent representation
+        """
+        parents = Comment.objects.order_by_votes().filter(
+                video_id=self.kwargs['video_id'], parent_id=None)
+        return parents
+
+    def get_context_data(self, **kwargs):
+        context = super(CommentListView, self).get_context_data(**kwargs)
+        context['video'] = Video.objects.get(id=self.kwargs['video_id'])
+        return context
